5969684c90ad03f73cf822a48b5865b70fdd0a3a
di3go-paz <diego.paz.maturana@gmail.com>
2025-03-29 19:51:19 -0300

    portafolio universidad

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,18 @@
 let operacion = "";
 let resultado = 0;
 
+function agregar(valor){
+    const entrada = document.querySelector("#entrada");
+    operacion += valor;
+    entrada.value = operacion;
+}
+
+const botones = document.querySelectorAll(".btn");
+
+botones.forEach(boton => {
+    boton.addEventListener("click", (event) => {
+        event.preventDefault();
+        const valor = boton.textContent;
+        agregar(valor);
+    });
+});
